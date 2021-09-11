956ff8676a30116dd63d9725f86e5912e32c0ace
surya dharamgudi <suryablaster@gmail.com>
2021-09-11 13:57:32 +0530

    new

--- lastocc.cpp
+++ lastocc.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int binarySearch(int a[],int n,int key){
-    int ans;
     int s=0;
     int e=n-1;
     while(s<=e){
         int mid=(s+e)/2;
-        if(a[mid]==key){
-            return mid;
-        }else if(key<a[mid]){
+        if(key<a[mid]){
             e=mid-1;
-        }else{
+        }else if(key>a[mid]) {
             s=mid+1;
+        }else{
+            if(mid==n-1 || a[mid]!=a[mid+1]){
+                return mid;
+            }else{
+                s=mid+1;
+            }
         }
     }
-
 
     return -1;
 }
