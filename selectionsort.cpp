c98122cdc775a3103fb7d369198482b37baccc12
surya dharamgudi <suryablaster@gmail.com>
2021-09-16 15:19:27 +0530

    new

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 void selectionsort(int a[],int n){
     for(int i=0;i<n-1;i++){
-        for(int j=i;j<n;j++){
-            
+        int mino = INT_MAX;
+        for(int j=i+1;j<n;j++){
+
+           if(a[j]<a[i]){
+               swap(a[j],a[i]);
+           }
+
+          
+
         }
     }
 }
