eef7d74ddf408e40a1335515aba7990ef366c8bc
surya dharamgudi <suryablaster@gmail.com>
2021-09-16 14:36:26 +0530

    new

---
+++ bubblesort.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+void bubblesort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(a[j] > a[j+1]){
+                swap(a[j],a[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+
+int n;
+cin>>n;
+
+int a[n];
+
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+
+bubblesort(a,n);
+
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+
+
+    return 0;
+}
