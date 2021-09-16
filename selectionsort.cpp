da42f2d06b557b0221f66e7eba32a7bca7353367
surya dharamgudi <suryablaster@gmail.com>
2021-09-16 14:48:44 +0530

    new

---
+++ selectionsort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void selectionsort(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i;j<n;j++){
+            
+        }
+    }
+}
+
+int main(){
+
+int n;
+cin>>n;
+
+int a[n];
+
+for(int i=0;i<n;i++){
+    cin>>a[i];
+}
+
+selectionsort(a,n);
+
+
+for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+}
+
+
+
+    return 0;
+}
