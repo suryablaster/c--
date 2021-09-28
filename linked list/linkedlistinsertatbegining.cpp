fed5eb945a7f737995f664624700e02f324eb997
surya dharamgudi <suryablaster@gmail.com>
2021-09-28 13:09:37 +0530

    new

--- linked list/linkedlistinsertatbegining.cpp
+++ linked list/linkedlistinsertatbegining.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-using namespace srd;
+using namespace std;
 
 class Node{
 
@@ -16,9 +16,38 @@ public:
 
 };
 
+void insertatbegining(Node* &head,int d){
+
+    if(head==NULL){
+        head=new Node(d);
+        return;
+    }
+
+    Node* n = new Node(d);
+    n->next=head;
+    head=n;
+
+
+}
+
+void print(Node* head){
+    if(head != NULL){
+        cout<<head->data;
+        head=head->next;
+    }
+}
+
 int in(){
 
+    Node* head=NULL;
 
+
+insertatbegining(head,3);
+insertatbegining(head,2);
+insertatbegining(head,1);
+insertatbegining(head,0);
+
+print(head);
 
 
 
