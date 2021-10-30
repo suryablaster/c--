8adfdbfa79c591448ae2efa8880dd7e5cf535252
surya dharamgudi <suryablaster@gmail.com>
2021-10-30 12:55:30 +0530

    new

---
+++ linked list/set1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printset(set<int> s){
+
+for(int key : s){
+    cout<<key;
+}
+
+
+
+
+
+
+
+
+}
+int main(){
+
+
+set<int> s;
+s.insert(1);
+s.insert(2);
+s.insert(3);
+s.insert(4);
+s.insert(5);
+s.insert(6);
+s.insert(7);
+s.insert(8);
+s.insert(9);
+s.insert(10);
+
+
+
+printset(s);
+
+
+
+
+
+
+    return 0;
+}
