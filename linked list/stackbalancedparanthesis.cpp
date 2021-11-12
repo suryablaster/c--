4c782d241e9133b56fe38f62aa33c78bcdf22d44
surya dharamgudi <suryablaster@gmail.com>
2021-11-12 21:17:11 +0530

    new

---
+++ linked list/stackbalancedparanthesis.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    bool matching(char a, char b){
+        return  ((a=='[' && b==']') || (a=='{' && b=='}') || (a=='(' && b==')'));
+    }
+    bool isValid(string s) {
+        
+        stack<char> st;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.size()== 0) return false;
+                else if ( matching(st.top(),s[i]) == false) return false;
+                else{
+                    st.pop();
+                }
+            }
+        }
+        return (st.size()==0);
+    }
+};
